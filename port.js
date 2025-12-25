76e310acc994e21c370a33e537bef8e88e39fb28
Anchal kushwaha <kushwahaanchal191@gmail.com>
2025-12-25 11:24:22 +0530

    pussing portfolio on github

---
+++ port.js
@@ -0,0 +1,34 @@
+ScrollReveal({
+    reset: true ,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+   });
+
+   ScrollReveal().reveal('.header', { delay: 500 , origin:'left'});
+   ScrollReveal().reveal('.info .div', { delay: 600 , origin:'right'});
+   ScrollReveal().reveal('.info .heading ', { delay: 700 , origin:'left'});
+   ScrollReveal().reveal('.info .para', { delay: 700 , origin:'bottom'});
+   ScrollReveal().reveal('.heading2', { delay: 800 , origin:'left'});
+   ScrollReveal().reveal('.about .para2', { delay: 900 , origin:'bottom'});
+   ScrollReveal().reveal('.about .img2', { delay: 900 , origin:'right'});
+   ScrollReveal().reveal('.heading3', { delay: 900 , origin:'top'});
+   ScrollReveal().reveal('.skills .div1', { delay: 900 , origin:'right'});
+   ScrollReveal().reveal('.skills .div2', { delay: 900 , origin:'right'});
+   ScrollReveal().reveal('.skills .div3', { delay: 900 , origin:'right'});
+   ScrollReveal().reveal('.skills .inndiv', { delay: 900 , origin:'left'});
+   ScrollReveal().reveal('.skills .inndiv2', { delay: 900 , origin:'left'});
+   ScrollReveal().reveal('.skills .inndiv3', { delay: 900 , origin:'left'});
+   ScrollReveal().reveal('.heading4', { delay: 900 , origin:'left'});
+   ScrollReveal().reveal('.sign .sign1', { delay: 1000 , origin:'top'});
+   ScrollReveal().reveal('.sign .sign2', { delay: 1000 , origin:'top'});
+   ScrollReveal().reveal('.sign .sign3', { delay: 1000 , origin:'top'});
+   ScrollReveal().reveal('.project .pro1', { delay: 1000 , origin:'bottom'});
+   ScrollReveal().reveal('.project .pro2', { delay: 1000 , origin:'bottom'});
+   ScrollReveal().reveal('.project .pro3', { delay: 1000 , origin:'bottom'});
+   ScrollReveal().reveal('.sociicon .soc1', { delay: 900 , origin:'bottom'});
+   ScrollReveal().reveal('.sociicon .soc2', { delay: 900 , origin:'bottom'});
+   ScrollReveal().reveal('.sociicon .soc3', { delay: 900 , origin:'bottom'});
+   ScrollReveal().reveal('.heading5', { delay: 900 , origin:'bottom'});
+
+
